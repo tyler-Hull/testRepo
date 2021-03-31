af858484ec083d938b24a1bc2a7feedd17c2122c
tyler-Hull <81710848+tyler-Hull@users.noreply.github.com>
2021-03-31 12:32:47 -0700

    Create testFile.c
    Test file for swapping two numbers.

---
+++ testFile.c
@@ -0,0 +1,25 @@
+// Simple program to swap two numbers
+// Example from https://www.programiz.com/c-programming/examples/swapping
+
+
+#include<stdio.h>
+int main() {
+      double first, second, temp;
+      printf("Enter first number: ");
+      scanf("%lf", &first);
+      printf("Enter second number: ");
+      scanf("%lf", &second);
+
+      // Value of first is assigned to temp
+      temp = first;
+
+      // Value of second is assigned to first
+      first = second;
+
+      // Value of temp (initial value of first) is assigned to second
+      second = temp;
+
+      printf("\nAfter swapping, firstNumber = %.2lf\n", first);
+      printf("After swapping, secondNumber = %.2lf", second);
+      return 0;
+}
